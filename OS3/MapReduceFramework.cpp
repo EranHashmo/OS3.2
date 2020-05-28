49bfd8e9a7106a4f223edd9ddc3873a0f2f028f5
owner <eran.chashmonay@mail.huji.ac.il>
2020-05-28 23:22:28 +0300

    changed stuff

--- OS3/MapReduceFramework.cpp
+++ OS3/MapReduceFramework.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 
 #include "MapReduceFramework.h"
 #include "MapReduceClient.h"
@@ -8,6 +7,7 @@
 #include <iostream>
 #include <atomic>
 #include <pthread.h>
+#include <algorithm>
 
 typedef void* JobHandle;
 
@@ -15,11 +15,13 @@ typedef void* JobHandle;
 struct ThreadContext
 {
     int id;
-    std::vector<IntermediatePair> *interVec;
+    void * c;
 };
 
 struct JobContext
 {
+//    pthread_t *threads;
+//    pthread_t shuffleThread;
     const MapReduceClient &_client;
     const InputVec &inVec;
     IntermediateMap *interMap;
@@ -27,27 +29,37 @@ struct JobContext
     JobState *_jobState;
     Barrier *_barrier;
     int _multiThreadLvl;
-    std::vector<ThreadContext*> tc;
+//    std::map<pthread_t*, ThreadContext*> tc;
+    std::vector<ThreadContext*> tcs;
     std::atomic<int> jobAtomic;
-    int runningThreadId;
+//    std::vector<int> finishedThreads;
+    std::atomic<int> finishedThreads;
+    std::atomic<int> runningThreadId;
 
     JobContext(const MapReduceClient &client, const InputVec &inputVec, OutputVec &outputVec, IntermediateMap *intermediateMap, JobState *jobState,
                Barrier *barrier, int multiThreadLevel)
             : _client(client), inVec(inputVec), outVec(outputVec), interMap(intermediateMap), _jobState(jobState),
               _barrier(barrier), _multiThreadLvl(multiThreadLevel), jobAtomic(0), runningThreadId(0)
     {
-        tc = std::vector<ThreadContext*>(multiThreadLevel);
+//        tc = std::vector<ThreadContext*>(multiThreadLevel);
+//        tc = std::map<pthread_t*, ThreadContext*>();
     }
 };
 
-
+bool key2Comp(IntermediatePair p1, IntermediatePair p2)
+{
+    return *p1.first < *p2.first;
+}
 
 
 void emit2 (K2* key, V2* value, void* context)
 {
-    auto* job = (JobContext*)context;
+//    auto* job = (JobContext*)context;
     IntermediatePair pair(key, value);
-    (job)->tc[job->runningThreadId]->interVec->push_back(pair);
+//    (job)->tc[job->runningThreadId]->interVec->push_back(pair);
+//    (job)->tc[pthread_self()]->interVec->push_back(pair);
+    auto *tc = (ThreadContext*)context;
+    ((std::vector<IntermediatePair> *)tc->c)->push_back(pair);
 }
 
 void emit3 (K3* key, V3* value, void* context)
@@ -57,47 +69,93 @@ void emit3 (K3* key, V3* value, void* context)
 
 void *startRoutine(void* arg)
 {
+
     auto* job = (JobContext*)arg;
     job->_jobState->stage = MAP_STAGE;
+
+    auto *threadVector = new std::vector<IntermediatePair>;
+    auto *tc = new ThreadContext{job->runningThreadId++, threadVector};
+
+
+    job->tcs.push_back(tc);
     int oldVal = 0;
-    while (true)
+//    if (job->runningThreadId == 0)
+    if (tc->id == 0)
     {
-        oldVal = (job->jobAtomic)++;
-        if (oldVal >= (job->inVec).size())
+        int id = 1;
+        while(job->finishedThreads != job->_multiThreadLvl and job->_jobState->percentage != 100)
         {
-            job->_jobState->percentage = 100;
-            break;
+            for (auto pair : *(std::vector<IntermediatePair>*)job->tcs[id]->c)
+            {
+
+                if (job->interMap->find(pair.first) == job->interMap->end())
+                {
+                    std::vector <V2 *> vector;
+                    (*job->interMap)[pair.first] = vector;
+                }
+                (*job->interMap)[pair.first].push_back(pair.second);
+            }
+            id = (id + 1) % job->_multiThreadLvl;
+            if (id == 0)
+            {
+                id = (id + 1)% job->_multiThreadLvl;
+            }
+
+//            for (auto item: *(job->tcs)[job->runningThreadId]->interVec)
+//            {
+//                // maybe it would be better in some other function? so this one will be smaller.
+//                // todo add something about percentage and maybe atomic variable
+//                K2 *key =item.first;
+//                auto it = (job ->interMap)->find(key);
+//                if (it == job->interMap->end())
+//                {
+//                    std::vector <V2 *> vector;
+//                    vector.push_back(item.second);
+//                    std::pair <K2*, std::vector<V2 *>> pair(item.first, vector);
+//                    job->interMap->insert(pair);
+//                }
+//                else
+//                {
+//                    it->second.push_back(item.second);
+//                }
+//            }
         }
-        job->_client.map(job->inVec[oldVal].first, job->inVec[oldVal].second, job);
-        job->_jobState->percentage = (float)(oldVal / job->inVec.size()) * 100;
+
+    }
+    else
+        {
+        while (true)
+        {
+            oldVal = (job->jobAtomic)++;
+            if (oldVal >= (job->inVec).size())
+            {
+                job->_jobState->percentage = 100;
+                break;
+            }
+//            job->_client.map(job->inVec[oldVal].first, job->inVec[oldVal].second, job);
+            job->_client.map(job->inVec[oldVal].first, job->inVec[oldVal].second, tc);
+            job->_jobState->percentage = (float)(oldVal / job->inVec.size()) * 100;
+        }
+//        job->finishedThreads.push_back(job->runningThreadId);
+        job->finishedThreads++;
     }
 
-    job->_barrier->barrier();
+    // Sort??
+//    std::sort(*(job->tc)[job->runningThreadId]->interVec->begin(),
+//            *(job->tc)[job->runningThreadId]->interVec->end(), key2Comp);
+
 
     //  Shuffle
     job->_jobState->stage = SHUFFLE_STAGE;
     job->_jobState->percentage = 0.0;
-    for (auto item: *(job->tc)[job->runningThreadId]->interVec)
-    {
-        // maybe it would be better in some other function? so this one will be smaller.
-        // todo add something about percentage and maybe atomic variable
-        K2 *key =item.first;
-        auto it = (job ->interMap)->find(key);
-        if (it == job->interMap->end())
-        {
-            std::vector <V2 *> vector;
-            vector.push_back(item.second);
-            std::pair <K2*, std::vector<V2 *>> pair(item.first, vector);
-            job->interMap->insert(pair);
-        }
-        else
-        {
-            it->second.push_back(item.second);
-        }
-    }
-    // Sort??
+
+    delete (std::vector<IntermediatePair>*)tc->c;
+
+    job->_barrier->barrier();
 
     // Reduce
+    job->_jobState->stage = SHUFFLE_STAGE;
+    job->_jobState->percentage = 0.0;
 
     return 0;
 }
@@ -105,29 +163,31 @@ void *startRoutine(void* arg)
 JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel)
 {
     printf("start Map reduce Job\n");
-    pthread_t threads[multiThreadLevel];
+    std::vector<pthread_t*> threads(multiThreadLevel);
+
     ThreadContext tcs[multiThreadLevel];
 
     auto *jobContext = new JobContext(client, inputVec, outputVec, new IntermediateMap(),
                                       new JobState({UNDEFINED_STAGE, 0}),
                                       new Barrier(multiThreadLevel), multiThreadLevel);
 
-
+//    jobContext->shuffleThread = &threadsArr[0];
     for (int i = 0; i < multiThreadLevel - 1; ++i)
     {
         jobContext->runningThreadId = i;
-        pthread_create(threads + i, NULL, startRoutine, jobContext);
+        pthread_create(threads[i], NULL, startRoutine, jobContext);
     }
 
-    std::vector<V2 *> v2Vec;
+
+//    std::vector<V2 *> v2Vec;
 //    for (auto item: interMap)
 //    {
 //        v2Vec.push_back(item.second);
 //    }
 
-    client.reduce(nullptr, v2Vec, nullptr);
+//    client.reduce(nullptr, v2Vec, nullptr);
 
-    return (JobHandle*)jobContext;
+    return jobContext;
 }
 
 void waitForJob(JobHandle job);
--- OS3/main.cpp
+++ OS3/main.cpp
@@ -1,28 +1,114 @@
 //
 // Created by owner on 5/25/2020.
+#include <atomic>
 #include "MapReduceFramework.h"
 #include "MapReduceClient.h"
 #include "IncClient.cpp"
+#include "Barrier.h"
 
+#include <iostream>
+
+struct ThreadContext
+{
+    int id;
+    void *c;
+    pthread_mutex_t tMut = PTHREAD_MUTEX_INITIALIZER;
+};
+
+struct Job
+{
+    pthread_t* threads;
+    int _threadNum;
+    std::atomic<int> _threadServed;
+    std::atomic<int> shuffleCounter;
+    std::atomic<int> counter;
+    Barrier b;
+    bool done;
+    ThreadContext *_tcs;
+    pthread_mutex_t *mut;
+
+    std::vector<int>& _inVec;
+    std::map<int, void *> interMap;
+    std::vector<int> outVec;
+
+    Job(int threadNum, ThreadContext* tcs, std::vector<int> &inVec)
+            : threads(new pthread_t[threadNum]), _threadNum(threadNum), _inVec(inVec) ,
+              shuffleCounter(0), counter(0), done(false), b(threadNum), _tcs(tcs), _threadServed(0)
+    {
+//        _inVec = inVec;
+        mut = new pthread_mutex_t;
+//        pthread_mutex_init(mut, nullptr);
+    }
+};
+
+void *threadRun(void * arg)
+{
+    Job *job = (Job*)arg;
+
+    auto *tc = new ThreadContext;
+    auto *c = new std::vector<int>;
+    tc->c = c;
+    tc->id = job->_threadServed++;
+
+    int oldVal = 0;
+    while(true)
+    {
+
+        oldVal = (job->counter)++;
+        if (oldVal >= (job->_inVec).size())
+        {
+            break;
+        }
+
+        ((std::vector<int> *)tc->c)->push_back(job->_inVec.at(oldVal) + 100);        //map
+        for (int i = 0; i < 1000000; i++){}
+
+    }
+    job->interMap[tc->id] = tc->c;
+    job->b.barrier();
+//    delete mapi;
+    job->done = true;
+    delete tc;
+    return nullptr;
+}
 
 int main(int argc, char *argv[])
 {
-    IncClient client;
-    InputVec inVec;
-    OutputVec outVec;
-    for (int i = 0; i < 20; i++)
+    int numOfThreads = 5;
+    auto *inVec = new std::vector<int>;
+    for (int i = 0; i< 20; i++)
     {
-        auto *j = new Intgrv(i);
-        inVec.push_back({nullptr, j});
+        inVec->push_back(i);
+    }
+    auto *threads = new pthread_t[numOfThreads];
+    auto *contexts = new ThreadContext[numOfThreads];
+
+    Job* job = new Job(numOfThreads, contexts, *inVec);
+    job->threads = threads;
+
+    for (int i = 0; i < numOfThreads; ++i)
+    {
+        pthread_create(threads + i, NULL, threadRun, job);
     }
 
-    startMapReduceJob(client, inVec, outVec, 3 );
-
-    printf("values: \n");
-    for (auto item : outVec)
+    for (int i = 0; i < numOfThreads; ++i)
     {
-        printf("%d\t", ((Intgrv*)(item.second))->_val);
+        pthread_join(threads[i], NULL);
     }
+
+    for (auto item: job->interMap)
+    {
+        printf("%d\n", item.first);
+        for (auto i: *(std::vector<int>*)item.second)
+        {
+            printf("%d:\t%d,\t", item.first, i);
+        }
+        printf("\n");
+    }
+
+    delete[] threads;
+    delete[] contexts;
+    delete job;
 
     return 0;
 }
